d573bd496ede08968b5b705c2a60a0596d8e82e7
glk12 <glopesaraujo6@gmail.com>
2026-03-09 16:20:17 -0300

    02

--- python02/ex1/ft_different_errors.py
+++ python02/ex1/ft_different_errors.py
@@ -1,18 +1,38 @@
-def garden_operations(value: str):
+def garden_operations(type: str):
     try:
-        int(value)
-        value / 0
-        open("missing.txt")
+        if type == "value":
+            print(int("abc"))
+        elif type == "zero":
+            print(10 / 0)
+        elif type == "file":
+            open("missing.txt")
+        elif type == "key":
+            mydict = {"key": "value"}
+            print(mydict["potential_key"])
     except ValueError:
         print("Caught ValueError: invalid literal for int()")
     except ZeroDivisionError:
         print("Caught ZeroDivisionError: division by zero")
     except FileNotFoundError as e:
-        print(e)
+        print(f"Caught FileNotFoundError: no such file '{e.filename}'")
+    except KeyError as e:
+        print(f"Caught KeyError: {e.}")
 
 
 def test_error_types():
-    print("Testing")
+    print("=== Garden Error Types Demo ===")
+
+    print("Testing ValueError...")
+    garden_operations("value")
+
+    print("Testing ZeroDivisionError...")
+    garden_operations("zero")
+
+    print("Testing FileNotFoundError...")
+    garden_operations("file")
+
+    print("Testing KeyError...")
+    garden_operations("key")
 
 
 if __name__ == "__main__":
